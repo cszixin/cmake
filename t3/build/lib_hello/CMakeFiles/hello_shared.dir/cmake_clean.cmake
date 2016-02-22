fb8579f2760a99361c6a395a5e5291ed3b2e51bf
cszixin <844952162@qq.com>
2016-02-22 14:52:56 +0800

    cmake_example

---
+++ t3/build/lib_hello/CMakeFiles/hello_shared.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_shared.dir/hello.cpp.o"
+  "../lib/libhello.pdb"
+  "../lib/libhello.dylib"
+  "../lib/libhello.1.2.dylib"
+  "../lib/libhello.1.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ t4/build/src/CMakeFiles/t4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/t4.dir/main.cpp.o"
+  "../bin/t4.pdb"
+  "../bin/t4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/t4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
